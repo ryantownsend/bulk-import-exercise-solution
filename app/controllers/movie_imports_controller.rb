0bb5903884d7ad0159313768b7a893e211852dfd
Ryan Townsend <ryan@ryantownsend.co.uk>
2022-05-15 20:27:33 +0100

    Avoid parsing JSON in Ruby

--- app/controllers/movie_imports_controller.rb
+++ app/controllers/movie_imports_controller.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
-class MovieImportsController < ApplicationController
+# we've moved to ActionController::Metal as this doesn't trigger the middleware
+# for parsing JSON into an object, allowing us to push the unparsed JSON
+# straight into ActiveRecord, reducing CPU and memory overhead
+class MovieImportsController < ActionController::Metal
+  # we have to include some modules to keep our clean JSON response rendering
+  include AbstractController::Rendering
+  include ActionController::Rendering
+  include ActionController::Renderers
+  use_renderers :json
+
   def create
     @movie_import = MovieImport.create(entries: movie_import_entries)
 
@@ -9,13 +18,20 @@ class MovieImportsController < ApplicationController
     else
       render json: { errors: @movie_import.errors.full_messages }, status: :unprocessable_entity
     end
-  rescue ActionDispatch::Http::Parameters::ParseError
-    render json: { errors: ["Entries contains invalid JSON"] }, status: :unprocessable_entity
+  rescue ActiveRecord::StatementInvalid => e
+    if e.cause.is_a?(PG::InvalidTextRepresentation)
+      render json: { errors: ["Entries contains invalid JSON"] }, status: :unprocessable_entity
+    else
+      raise e
+    end
   end
 
   private
 
   def movie_import_entries
-    params.permit("_json": [:id, :title, :description, :rating, :published, subscriber_emails: []])["_json"]
+    # we've replaced accessing the JSON via strong parameters to using the raw
+    # post body from the request - be aware this may have security implications
+    # in your own code
+    request.raw_post
   end
 end
--- app/models/movie_import.rb
+++ app/models/movie_import.rb
@@ -1,4 +1,10 @@
 class MovieImport < ApplicationRecord
+  # attributes
+  # we tell Rails to treat the entries as text instead of JSONB, this way it
+  # won't perform expensive typecasting/parsing between Ruby objects and the
+  # raw JSON string
+  attribute :entries, :text
+
   # callbacks
   after_commit :trigger_import, on: :create
 
